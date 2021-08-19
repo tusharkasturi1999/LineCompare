8e34e0bfeaae5a80260304334d2579447a41e2c0
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 12:14:58 +0530

    Added UC1

--- src/com/LineCompare/Linecompare.java
+++ src/com/LineCompare/Linecompare.java
@@ -1,8 +1,36 @@
 package com.LineCompare;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Linecompare {
 	public static void main( String[] args ) {
 		System.out.println("Welcome to Line Comparision");
+		
+		Scanner sc = new Scanner(System.in);
+		
+	    System.out.println("Enter the value of x1");
+	    int x1 = sc.nextInt();
+	    System.out.println("x1 is: " + x1);
+	    
+	    System.out.println("Enter the value of y1");
+	    int y1 = sc.nextInt();
+	    System.out.println("y1 is: " + y1);
+	 
+	    System.out.println("Enter the value of x2");
+	    int x2 = sc.nextInt();
+	    System.out.println("x2 is: " + x2);
+	    
+	    System.out.println("Enter the value of y2");
+	    int y2 = sc.nextInt();
+	    System.out.println("y2 is: " + y2);
+	    
+	    int x3=(x2-x1);
+	    int y3=(y2-y1);
+	    double lengthOfaLine=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
+	    System.out.println("length of Line 1 :" +lengthOfaLine);
+	    
+	    
+		
 	}
 
 }

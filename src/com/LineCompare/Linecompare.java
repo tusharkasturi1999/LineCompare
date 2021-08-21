e0ec1c24c8b98b4be732a9584288314b574754d5
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-21 16:14:11 +0530

    Added UC4 - Reconstructed code with OOP Concepts

--- src/com/LineCompare/Linecompare.java
+++ src/com/LineCompare/Linecompare.java
@@ -3,6 +3,39 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Linecompare {
+	public int x1,x2,x4,x5,y1,y2,y4,y5;
+	
+	Linecompare(int x1,int y1,int x2,int y2,int x4, int y4, int x5, int y5){
+		this.x1 =x1;
+		this.x2 =x2;
+		this.x4 =x4;
+		this.x5 =x5;
+		this.y1 =y1;
+		this.y2 =y2;
+		this.y4 =y4;
+		this.y5 =y5;
+	}
+	public void Comparing(){
+		int x3=(x2-x1);
+		int y3=(y2-y1);
+		Double lengthOfaLine1=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
+		System.out.println("length of Line 1 :" +lengthOfaLine1);
+		
+	    int x6=(x5-x4);
+	    int y6=(y5-y4);
+	    Double lengthOfaLine2=Math.sqrt((Math.pow(x6, 2))+(Math.pow(y6, 2)));
+	    System.out.println("length of Line 2 :" +lengthOfaLine2);
+	    
+	 /*   if(lengthOfaLine1 == lengthOfaLine2) {
+	    	System.out.println("Length of two lines are Equal");
+	    } */
+	
+	    int op= lengthOfaLine1.compareTo(lengthOfaLine2);
+	    if(op==0)
+	    System.out.println("Twon lines are equal");
+	}
+	
+	
 	public static void main( String[] args ) {
 		System.out.println("Welcome to Line Comparision");
 		
@@ -24,11 +57,6 @@ public class Linecompare {
 	    int y2 = sc.nextInt();
 	    System.out.println("y2 is: " + y2);
 	    
-	    int x3=(x2-x1);
-	    int y3=(y2-y1);
-	    Double lengthOfaLine1=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
-	    System.out.println("length of Line 1 :" +lengthOfaLine1);
-	    
 	    System.out.println("Enter the value of x4");
 	    int x4 = sc.nextInt();
 	    System.out.println("x4 is: " + x4);
@@ -45,19 +73,10 @@ public class Linecompare {
 	    int y5 = sc.nextInt();
 	    System.out.println("y5 is: " + y5);
 	    
-	    int x6=(x5-x4);
-	    int y6=(y5-y4);
-	    Double lengthOfaLine2=Math.sqrt((Math.pow(x6, 2))+(Math.pow(y6, 2)));
-	    System.out.println("length of Line 2 :" +lengthOfaLine2);
-	    
-	 /*   if(lengthOfaLine1 == lengthOfaLine2) {
-	    	System.out.println("Length of two lines are Equal");
-	    } */
-	
-	    int op= lengthOfaLine1.compareTo(lengthOfaLine2);
-	    if(op==0)
-	    System.out.println("Twon lines are equal");
+	    sc.close();
 		
+	    Linecompare obj = new Linecompare(x1,y2,x2,y2,x4,y4,x5,y5);
+	    obj.Comparing();
 	}
 
 }
